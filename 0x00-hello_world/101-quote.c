9da85633680988d17541afd532b5578093ba833e
mulandi7 <mulandi.mangoka@gmail.com>
2023-02-16 15:05:59 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *main - A c program that prints a line to the standard error
- *Return: 1
+ * main - A c program that prints a line to the standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout)
+		return (1);
 }
