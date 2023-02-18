7de43b88a5901cade2c7f651cbdfe2f6f6823dea
mulandi7 <mulandi.mangoka@gmail.com>
2023-02-17 16:31:36 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 
 /**
- *main - prints all possible combinations of two two-digit numbers,
- *        ranging from 0-99, separated by a comma followed by a space.
+ * main - prints all combs of three different digits,
+ *        in ascending order, comma separated, followed by a space
  *
  * Return: 0
  */
 int main(void)
 {
-	int i, j;
+	int digit1, digit2, digit3;
 
-	for (i = 0; i < 100; i++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-	for (j = 0; j < 100; j++)
-	{
-		if (i < j)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			putchar((i / 10) + 48);
-			putchar((i % 10) + 48);
-			putchar(' ');
-			putchar((j / 10) + 48);
-			putchar((j % 10) + 48);
-		if (i != 98 || j != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
-	}
+
 	putchar('\n');
+
 	return (0);
 }
